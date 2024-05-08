4af2dfe5fcfbb97b7ee87b8c13154f0844974ec7
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-05-08 09:40:00 +0300

    Initialized the project

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Yukawe</h1>
+      <h2>Our Pictures</h2>
+      <p>Explore our lil life.</p>
+    </div>
+  )
+}
+
+export default Title;
