45324245625d989d3631f262f9e683af0aef53ab
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-05-08 10:10:16 +0300

    Added upload functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './index.css';
 import Title from './components/Title';
+import Upload from './components/Upload';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Upload />
     </div>
   );
 }
---
+++ src/components/Upload.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const Upload = () => {
+    const [file, setFile] = useState(null)
+    const types = ['image/png', 'image/jpeg', 'image/jpg']
+
+    const changeHandler = (e)=>{
+        
+        let selected = e.target.files[0]
+        console.log(selected)
+        if(selected && types.includes(selected.type) ){
+            setFile(selected)
+        } else{
+            setFile(null)
+            alert('Please select an image file (png or jpg)')
+        }
+
+    }
+  return (
+    <form>
+        <input type='file' onChange={changeHandler} />
+        <div className='output'>
+            {file && <div>{file.name} </div>}
+        </div>
+    </form>
+  )
+}
+
+export default Upload
