328df349be6c120a6b938391e16ce062e4f7b0d3
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-05-08 11:32:15 +0300

    Created display for images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './index.css';
 import Title from './components/Title';
 import Upload from './components/Upload';
+import Image from './components/Image';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <Upload />
+      <Image />
     </div>
   );
 }
