7f00f6d7407d80861f32edcfa13b1a396ac507cf
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-05-08 12:04:53 +0300

    Added modal

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './index.css';
 import Title from './components/Title';
 import Upload from './components/Upload';
 import Image from './components/Image';
+import Modal from './components/Modal';
+import { useState } from 'react';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
       <Title />
       <Upload />
-      <Image />
+      <Image setSelectedImg={setSelectedImg} />
+      { selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
--- src/components/Image.js
+++ src/components/Image.js
@@ -2,14 +2,14 @@ import React from 'react'
 import useFirestore from '../hooks/useFirestore'
 import {motion} from 'framer-motion'
 
-const Image = () => {
+const Image = ({ setSelectedImg }) => {
     const {docs }= useFirestore('images')
     console.log(docs)
   return (
     <div className='img-grid'>
         {docs && docs.map(doc => (
             <motion.div className='img-wrap' key={doc.id}
-            whileHover={{opacity: 1}} layout>
+            whileHover={{opacity: 1}} layout onClick={() => setSelectedImg(doc.url)}>
                <motion.img src={doc.url} alt='uploaded' initial={{opacity:0}} 
                animate={{opacity:1}} transition={{delay:1}} />
              </motion.div>
